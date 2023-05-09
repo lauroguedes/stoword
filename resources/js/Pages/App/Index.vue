b62217387804d421651508274ca8ad096c12927d
Lauro Guedes <leowgweb@gmail.com>
2023-05-09 17:18:28 -0300

    feat(layout): create the auth app layout and home page

---
+++ resources/js/Pages/App/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <Head title="App" />
+    <AuthAppLayout>
+        <WordField />
+    </AuthAppLayout>
+</template>
+
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import AuthAppLayout from "@/Layouts/AuthAppLayout.vue";
+import WordField from "@/Components/App/WordField.vue";
+</script>
