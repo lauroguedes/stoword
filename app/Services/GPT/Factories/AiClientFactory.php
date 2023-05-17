48c77c71cbe2c1e35e8f49d7a9096d53dd644390
Lauro Guedes <leowgweb@gmail.com>
2023-05-17 12:07:21 -0300

    test: create tests for AiClientFactory

--- app/Services/GPT/Factories/AiClientFactory.php
+++ app/Services/GPT/Factories/AiClientFactory.php
@@ -11,16 +11,14 @@ class AiClientFactory implements GptFactoryContract
     public function factory(GptModelTypes|string $modelType): AiClientContract
     {
         if (is_string($modelType)) {
+            throw_if(!GptModelTypes::tryFrom($modelType), new \ValueError('Invalid model expected'));
+
             $modelType = GptModelTypes::from($modelType);
         }
 
         $classInstance = app($modelType->getModelConcreteClass());
 
-        abort_unless(
-            $classInstance instanceof AiClientContract,
-            400,
-            'Invalid Ai Client Service'
-        );
+        throw_unless($classInstance instanceof AiClientContract, new \Exception('Invalid AI Client Service'));
 
         return $classInstance;
     }
