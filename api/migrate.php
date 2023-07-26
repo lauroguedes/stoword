5b025bb70ec0e0ba7ed61531e32f62ca656e1505
Lauro Guedes <leowgweb@gmail.com>
2023-07-26 09:01:17 -0300

    feat(config): add migrate command on composer

--- api/migrate.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-chdir(__DIR__ . '/..'); // Move one directory up to the root of the Laravel project
-
-// Include the Composer autoload file to access Laravel classes
-require 'vendor/autoload.php';
-
-// Run Laravel migrations
-\Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
