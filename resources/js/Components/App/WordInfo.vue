2edc129bdb23b308cc87619e6d4607e3ec646e6f
Lauro Guedes <leowgweb@gmail.com>
2023-09-09 20:27:45 -0300

    feature(interface): add new information section for word

---
+++ resources/js/Components/App/WordInfo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="my-5 text-white">
+        <div class="flex mb-2 justify-center gap-3 items-center text-3xl">
+            <div class="text-gray-300">{{ data.word }}</div>
+            <div class="mx-1">|</div>
+            <div class="text-gray-400">{{ data.ipa_word }}</div>
+        </div>
+        <div
+            class="flex flex-col justify-center text-center text-lg text-gray-400 space-x-1"
+        >
+            <div v-if="data.plural">
+                <span class="uppercase text-sm">Plural: </span
+                ><span class="font-semibold">{{ data.plural }}</span>
+            </div>
+            <div v-if="data.synonyms">
+                <span class="uppercase text-sm">Synonyms: </span
+                ><span class="font-semibold">{{ data.synonyms }}</span>
+            </div>
+            <div v-if="data.word_forms">
+                <span class="uppercase text-sm">Other forms: </span
+                ><span class="font-semibold">{{ data.word_forms }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const data = computed(() => store.state.wordResponse);
+</script>
---
+++ resources/js/Components/App/WordMean.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="p-5 bg-gray-200 dark:bg-gray-800 rounded-md shadow">
+        <div
+            class="relative text-gray-600 dark:text-gray-400 font-bold text-lg mb-2"
+        >
+            <div class="absolute -top-9 -right-9">
+                <span
+                    class="inline-flex items-center gap-x-2 rounded-full bg-green-600/20 px-2.5 py-1 text-sm font-semibold leading-5 text-green-600"
+                >
+                    <span
+                        class="inline-block h-1.5 w-1.5 rounded-full bg-green-600"
+                    ></span>
+                    {{ data.part_of_speech }}
+                </span>
+            </div>
+            <h1 class="uppercase font-bold">Mean</h1>
+            <div>
+                {{ data.meaning.value }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const data = computed(() => store.state.wordResponse);
+</script>
