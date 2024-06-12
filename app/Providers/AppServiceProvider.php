17ae6ab9e9d448f22659a1d1930b9ea505a3998d
Lauro Guedes <leowgweb@gmail.com>
2024-06-12 21:36:26 +0100

    feat(di): Create dependency injection to the tts service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\TextToSpeach\TextToSpeachContract;
+use App\Services\TextToSpeach\TextToSpeachType;
+use App\Services\TextToSpeach\VoiceRss\VoiceRSS;
+use App\Services\TextToSpeach\VoiceRss\VoiceRssAdapter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(VoiceRssAdapter::class, function ($app) {
+            return new VoiceRssAdapter(new VoiceRSS());
+        });
+
+        $this->app->bind(TextToSpeachContract::class, function () {
+            $adapter = TextToSpeachType::from(config('tts.service'))->getAdapter();
+
+            return $this->app->make($adapter);
+        });
     }
 
     /**
---
+++ config/tts.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Text to Speach Config
+    |--------------------------------------------------------------------------
+    | This file is for storing the configuration for the text to speach service.
+    */
+
+    'service' => env('TTS_SERVICE', 'voice_rss'),
+];
